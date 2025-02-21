9f6a4f1fd6b3b0a38b7f3cde24fd4d28526c2b82
HTML <iamhtml666@gmail.com>
2025-02-21 03:36:47 +0300

    WIP fixing errors

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    root: "./", // Explicitly set root to project directory
-    publicDir: "public", // Ensure public folder is served
+    // root: "./",
+    // publicDir: "public",
+    server: {
+        open: true,
+    },
 });
