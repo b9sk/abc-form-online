23129cbb6b76da175c21bf9c93436e2f19b07f96
HTML <iamhtml666@gmail.com>
2025-02-21 04:39:05 +0300

    fix deploy base name

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    // root: "./",
-    // publicDir: "public",
     server: {
         open: true,
     },
+    base: "/abc-form-online/", // Replace with your repo name
 });
