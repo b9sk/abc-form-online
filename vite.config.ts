3b39c529acae6ede623081b04ea885e77fed67f0
HTML <iamhtml666@gmail.com>
2025-02-21 03:00:33 +0300

    package.json fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
+    root: "./", // Explicitly set root to project directory
+    publicDir: "public", // Ensure public folder is served
 });
